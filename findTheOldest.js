9915e08746b4682528ba6cc6197e2afff5f49a40
Brock <tammeusb@gmail.com>
2024-05-12 20:27:06 -0400

    Add Odin solution
    arr.reduce() is not only used to combine - its essential
    function is to *reduce* the array to a single item.
    In the Odin solution, the reduce function is called
    to sort the items by returning the highest age
    as the total for each iteration.

--- findTheOldest.js
+++ findTheOldest.js
@@ -1,12 +1,33 @@
-const findTheOldest = function(people) {
-    const sortedByAge = people.sort((a,b) => {
-        if ('yearOfDeath' in a === false) a.yearOfDeath = new Date().getFullYear();
-        if ('yearOfDeath' in b === false) b.yearOfDeath = new Date().getFullYear();
+/*const findTheOldest = function(people) {
+    const sortByAge = people.sort((a,b) => {
+        const currentDate = new Date().getFullYear();
+
+        if ('yearOfDeath' in a === false) a.yearOfDeath = currentDate;
+        if ('yearOfDeath' in b === false) b.yearOfDeath = currentDate;
+
         if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) return 1
         if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) return -1
     })
     return sortedByAge[sortedByAge.length - 1]
-};
+};*/
+
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
 
 
 //iterate through object, 
